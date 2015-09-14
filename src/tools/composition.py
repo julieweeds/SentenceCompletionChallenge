c2bc1291817a6e0b001ba0e5239c015927c65fd9
julieweeds <julie.weeds@gmail.com>
2015-09-14 11:33:10 +0100

    including yaml

--- src/tools/composition.py
+++ src/tools/composition.py
@@ -13,7 +13,7 @@ __author__ = 'juliewe'
 
 
 import sys,math,gzip
-import yaml  #this needs to be imported for composition but doesn't appear possible to install on disco machine
+import yaml
 import json
 
 from operator import itemgetter
@@ -122,8 +122,8 @@ class Composition:
         #load and configure
         print "Reading configuration from "+filename
         with open(filename) as fp:
-            config=json.load(fp,encoding="ascii")
-
+            #config=json.load(fp,encoding="ascii")
+            config=yaml.safe_load(fp)
 
         print config
         self.option=config.get("option","none")
