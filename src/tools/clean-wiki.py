a0922c841f9c3080839dceaff040faae82d42a22
julieweeds <julie.weeds@gmail.com>
2015-11-12 13:27:05 +0000

    clean up and chunk wiki before parsing

--- src/tools/clean-wiki.py
+++ src/tools/clean-wiki.py
@@ -3,7 +3,7 @@ __author__ = 'juliewe'
 import re
 
 linePATT = re.compile('wikipedia-\d+,\"(.*)\"')
-linePATT2 = re.compile('wikipedia-\d,(.*)')
+linePATT2 = re.compile('wikipedia-\d+,(.*)')
 
 def chunkNclean(infile,outdir,chunksize=100):
     with open(infile) as instream:
@@ -21,7 +21,7 @@ def chunkNclean(infile,outdir,chunksize=100):
             line =line.rstrip()
             matchobj=linePATT.match(line)
             if matchobj:
-                cleanline=matchobj.group(0)
+                cleanline=matchobj.groups()[0]
                 outstream.write(cleanline+"\n")
             else:
                 matchobj=linePATT2.match(line)
